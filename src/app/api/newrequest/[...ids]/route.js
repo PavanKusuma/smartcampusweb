fb43cc6aad4c3e18e3b84f7be8e73751fd7ab15b
Pavan Kusuma <newtonpavan33@gmail.com>
2023-04-29 15:19:21 +0530

    New request changes1

--- src/app/api/newrequest/[...ids]/route.js
+++ src/app/api/newrequest/[...ids]/route.js
@@ -21,7 +21,7 @@ export async function GET(request,{params}) {
 
             try {
                 // create query for insert
-                const q = 'INSERT INTO request (requestId, requestType, oRequestId, collegeId, description, requestFrom, requestTo, duration, requestStatus, requestDate, approver, approverName, approvedOn, comment, issuer, issuerName, issuedOn, isOpen, isStudentOut, returnedOn, isAllowed) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )';
+                const q = 'INSERT INTO request (requestId, requestType, oRequestId, collegeId, description, requestFrom, requestTo, duration, requestStatus, requestDate, approver, approverName, approvedOn, comment, issuer, issuerName, issuedOn, isOpen, isStudentOut, returnedOn, isAllowed) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )';
                 // create new request
                 const [rows, fields] = await connection.execute(q, [ params.ids[1], params.ids[2], params.ids[3], params.ids[4], params.ids[5], params.ids[6], params.ids[7], params.ids[8], "Submitted", params.ids[10] ,  '-','-', null, '-', '-','-',null, 1, 0, null, params.ids[9]]);
                 connection.release();
